97909f345d0b3635691240ae3935b4df19319145
Shine <47127993+Tidlsss@users.noreply.github.com>
2019-11-22 11:21:14 +0900

    2019-11-12 BOJ

--- BOJ-15552(백준 빠른 A+B).cpp
+++ BOJ-15552(백준 빠른 A+B).cpp
@@ -1,3 +1,6 @@
+//2019-11-12 BOJ-15552(빠른 A+B)
+//C++에서의 입출력 함수인 cin/cout는 stdio와 동기화를 하기때문에 시간이 걸린다.
+//이 동기화를 풀어줌으로써 scanf/printf만큼은 아니지만 비슷한 속도로 끌어올릴 수 있다.
 using namespace std;
 #include <iostream>
 
