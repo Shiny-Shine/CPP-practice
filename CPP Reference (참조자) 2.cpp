7c94695853b1df426178b240550c7b39ff07b1c1
Shine <47127993+Tidlsss@users.noreply.github.com>
2020-03-24 20:39:06 +0900

    2020-03-23 CPP Practice

--- CPP Reference (참조자) 2.cpp
+++ CPP Reference (참조자) 2.cpp
@@ -1,8 +1,14 @@
 /*
 	2020-03-23 C++ Practice
 
-	Reference(������, &) 2
+	Reference(참조자, &) 2
 
+	Call By Reference
+	참조자를 함수의 매개변수로도 활용이 가능하다.
+	C언어에서 포인터로 Call By Reference 처리를 했다면 (SWAP 함수 등)
+	C++ 에서는 참조자를 통해서 CBR 처리한다
+
+	참조자 변수로 구성된 함수 호출 시에는 반드시 변수이름이 들어가야 한다.
 */
 
 #include <iostream>
@@ -22,7 +28,7 @@ int main(void)
 	cout << "---Before Swapping---\n";
 	cout << "val1 : " << val1 << ", val2 : " << val2 << '\n';
 
-	SwapByRef(val1, val2);
+	SwapByRef(val1, val2);	//값이 들어오면 안됨.
 
 	cout << "---After Swapping---\n";
 	cout << "val1 : " << val1 << ", val2 : " << val2 << '\n';
