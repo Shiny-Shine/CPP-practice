b5e5c1f2294cc717071248ccd2c5d94eba69d8dd
Shine <47127993+Tidlsss@users.noreply.github.com>
2021-02-18 13:28:19 +0900

    2021-02-17 BOJ

---
+++ BOJ-17298(오큰수).cpp
@@ -0,0 +1,50 @@
+// 2021-02-17 BOJ - 17298(오큰수)
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+using namespace std;
+
+int freq[1000001];
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	stack<int> s;
+	int n;
+	cin >> n;
+
+	vector<int> arr(n);
+	vector<int> ans(n);
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+
+	}
+
+	s.push(0);
+	for (int i = 1; i < n; i++)
+	{
+		if (s.empty())
+			s.push(i);
+
+		while (!s.empty() && arr[s.top()] < arr[i])
+		{
+			ans[s.top()] = arr[i];
+			s.pop();
+		}
+
+		s.push(i);
+	}
+
+	while (!s.empty())
+	{
+		ans[s.top()] = -1;
+		s.pop();
+	}
+
+	for (int i = 0; i < n; i++)	cout << ans[i] << ' ';
+}
